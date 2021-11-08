986fa115e25a0df44d459cdda412af3348f450c7
h-ayub <ayub.humza@gmail.com>
2021-11-07 19:11:32 -0600

    Final Commit

--- app/src/main/java/com/example/lab_6_maps_activity/MainActivity.java
+++ app/src/main/java/com/example/lab_6_maps_activity/MainActivity.java
@@ -1,14 +1,86 @@
 package com.example.lab_6_maps_activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.security.Permissions;
+
 public class MainActivity extends AppCompatActivity {
+    private final LatLng mDestinationLatLng = new LatLng(43.0753747, -89.4044994);  // Bascom Hall
+    private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 12;
+    private LatLng device;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_map);
+
+        mapFragment.getMapAsync(googleMap -> {
+            mMap = googleMap;
+            // code to display marker
+            mMap.addMarker(new MarkerOptions().position(mDestinationLatLng).title("Destination"));
+
+            // Obtain a FusedLocationProviderClient
+            mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+            displayMyLocation();
+        });
+
+    }
+
+    private void displayMyLocation() {
+        // Check if permission granted
+        int permission = ActivityCompat.checkSelfPermission(this.getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permission == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        } else {
+            //mFusedLocationProviderClient.getCurrentLocation(100,null)
+            mFusedLocationProviderClient.getLastLocation()
+                    .addOnCompleteListener(this, task -> {
+                        Location mLastKnownLocation = task.getResult();
+                        if (task.isSuccessful() && mLastKnownLocation != null) {
+                            device = new LatLng((mLastKnownLocation.getLatitude()), mLastKnownLocation.getLongitude());
+                            mMap.addPolyline(new PolylineOptions().add(
+                                    device,
+                                    mDestinationLatLng));
+                            mMap.addMarker(new MarkerOptions().position(device).title("Device"));
+                        }
+                    });
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                displayMyLocation();
+            }
+        }
     }
 }
